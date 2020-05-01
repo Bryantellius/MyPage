28aa0ebdf09ec1fa3c5d31e6f3358ac0c8fc30a2
Ben Bryant <brbryant2639@gmail.com>
2020-05-01 09:30:32 -0500

    Good progress. Added menu svg, tweaked lines to create down arrow svg, added row under navbar that contains ul of react navlinks, menu hides/shows navlinks onclick, added spin and change feature to menu svg that spins when hovered over, and changes to arrow when clicked

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./views/Home";
 import About from "./views/About";
@@ -10,6 +10,19 @@ const App: React.FC<IAppProps> = () => {
     <main className="container-fluid bg-secondary w-100 h-100">
       <BrowserRouter>
         <NavBar />
+        <div id="nav-list" className="d-flex justify-content-end align-items-center">
+          <ul className="nav">
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/xp">Experience</NavLink>
+            </li>
+          </ul>
+        </div>
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route path="/about" component={About}></Route>
--- src/client/components/Nav.tsx
+++ src/client/components/Nav.tsx
@@ -1,33 +1,73 @@
 import * as React from "react";
-import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Nav: React.FC<INavProps> = () => {
+  const location = useLocation();
   const [menu, setMenu] = React.useState<boolean>(false);
 
   const toggleMenu = () => {
     if (!menu) {
-      document.getElementById("navLinks").style.zIndex = "4";
-      document.getElementById("navDivHome").style.opacity = "100%";
-      document.getElementById("navDivAbout").style.opacity = "100%";
-      document.getElementById("navDivXp").style.opacity = "100%";
+      document.getElementById("nav-list").style.height = "2rem";
+      document.getElementById("menuIcon").style.display = "none";
+      document.getElementById("arrowIcon").style.display = "inline";
       setMenu(true);
     } else {
-      document.getElementById("navDivHome").style.opacity = "0%";
-      document.getElementById("navDivAbout").style.opacity = "0%";
-      document.getElementById("navDivXp").style.opacity = "0%";
-      document.getElementById("navLinks").style.zIndex = "-1";
+      document.getElementById("nav-list").style.height = "0px";
+      document.getElementById("arrowIcon").style.display = "none";
+      document.getElementById("menuIcon").style.display = "inline";
       setMenu(false);
     }
   };
 
+  React.useEffect(() => {
+    document.getElementById("nav-list").style.height = "0px";
+    document.getElementById("arrowIcon").style.display = "none";
+    document.getElementById("menuIcon").style.display = "inline";
+    setMenu(false);
+  }, [location.pathname]);
+
   return (
     <>
       <div
-        id="nav"
-        className="col-md-4 border"
+        className="col-md-4 d-flex justify-content-end align-items-center border"
         onClick={toggleMenu}
       >
-        <span>Nav</span>
+        <svg
+          id="menuIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-menu"
+        >
+          {/* Menu icon */}
+          <line id="topLine" x1="3" y1="12" x2="21" y2="12"></line>
+          <line id="middleLine" x1="3" y1="6" x2="21" y2="6"></line>
+          <line id="bottomLine" x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+        <svg
+          id="arrowIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-menu"
+        >
+          {/* Down Arrow icon */}
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="12" y1="18" x2="21" y2="6"></line>
+          <line x1="3" y1="6" x2="12" y2="18"></line>
+        </svg>
       </div>
     </>
   );
