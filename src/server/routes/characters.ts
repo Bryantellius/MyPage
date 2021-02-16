8fd98812042f9b482ab13a54957544ce00246db3
Ben <brbryant2639@gmail.com>
2021-02-16 08:29:49 -0600

    Added temp endpoints for individual character requests

--- src/server/routes/characters.ts
+++ src/server/routes/characters.ts
@@ -1,11 +1,36 @@
 import * as express from "express";
+import * as fs from "fs";
 import * as path from "path";
+import characters from "../assets/characters.json";
 
 const router = express.Router();
 
 router.get("/characters", (req, res, next) => {
   try {
     res.sendFile(path.join(__dirname, "../src/server/assets/characters.json"));
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/characters/:name", (req, res, next) => {
+  try {
+    let name: string = req.params.name;
+    let json: JSON;
+    const data = fs.readFileSync(
+      path.join(__dirname, "../src/server/assets/characters.json"),
+      "utf8"
+    );
+    const { list } = JSON.parse(data.toString());
+    console.log(list);
+    console.log(name);
+    list.forEach((character: any) => {
+      if (name.toLowerCase() === character.name.toLowerCase()) {
+        json = character;
+      }
+    });
+    console.log(json);
+    res.send(json);
   } catch (err) {
     next(err);
   }
